c2857c74ed38e392fc88befdef68448c66004f99
lukasalencar <lucas.kauan@aluno.ifsp.edu.br>
2023-08-04 18:20:09 -0300

    api3

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,12 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const data = {
+  name: 'Renan Tinti',
+  age: 'novinho rs s2 -18',
+  array: [1, 2, 3, 4, 5],
+  map: [{id: 1, nome: 'Kleyton'}]
+}
 
 export default function handler(req, res) {
-  res.status(200).json(data)
-
-  const data = {
-    name: 'Renan Tinti',
-    age: 'novinho rs s2 -18',
-    array: [1, 2, 3, 4, 5],
-    map: [{id: 1, nome: 'Kleyton'}]
-  }
+  res.json(data)
 }
